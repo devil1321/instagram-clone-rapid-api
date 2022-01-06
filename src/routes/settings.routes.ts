47cdf613a84e57ddcc5cac5cb53f8c25a932f80a
insane <insane@00-25-D3-42-8C-28.lazy.com>
2022-01-06 13:29:11 +0100

    fix settings routes

--- src/routes/settings.routes.ts
+++ src/routes/settings.routes.ts
@@ -4,26 +4,26 @@ import { Response, Request, Application } from 'express'
 const router = require('express').Router()
 
 router.get('/edit-profile',(req:Request,res:any)=>{
-    res.render('settings-views/edit-profile',{title:'Edit Profile'})
+    res.render('./settings-views/edit-profile',{title:'Edit Profile'})
 })
 
 router.get('/change-password',(req:Request,res:any)=>{
-    res.render('settings-views/change-password',{title:'Change Password'})
+    res.render('./settings-views/change-password',{title:'Change Password'})
 })
 
 router.get('/manage-access',(req:Request,res:any)=>{
-    res.render('settings-views/manage-access',{title:'Apps & Websites'})
+    res.render('./settings-views/manage-access',{title:'Apps & Websites'})
 })
 router.get('/email-and-sms',(req:Request,res:any)=>{
-    res.render('settings-views/email-sms',{title:'Email & SMS'})
+    res.render('./settings-views/email-sms',{title:'Email & SMS'})
 })
 
 router.get('/push-notifications',(req:Request,res:any)=>{
-    res.render('settings-views/push-notifications',{title:'Notifications'})
+    res.render('./settings-views/push-notifications',{title:'Notifications'})
 })
 
 router.get('/manage-contacts',(req:Request,res:any)=>{
-    res.render('settings-views/manage-contacts',{title:'Manage Contacts'})
+    res.render('./settings-views/manage-contacts',{title:'Manage Contacts'})
 })
 
 router.get('/privacy-and-security',(req:Request,res:any)=>{
@@ -31,18 +31,18 @@ router.get('/privacy-and-security',(req:Request,res:any)=>{
 })
 
 router.get('/login-activity',(req:Request,res:any)=>{
-    res.render('settings-views/login-activity',{title:'Login Activity'})
+    res.render('./settings-views/login-activity',{title:'Login Activity'})
 })
 router.get('/emails-from-instagram',(req:Request,res:any)=>{
-    res.render('settings-views/emails',{title:'Emails'})
+    res.render('./settings-views/emails',{title:'Emails'})
 })
 
 router.get('/help',(req:Request,res:any)=>{
-    res.render('settings-views/help',{title:'Help'})
+    res.render('./settings-views/help',{title:'Help'})
 })
 
 router.get('/switch-to-proffesional-account',(req:Request,res:any)=>{
-    res.render('settings-views/switch-to-proffessional-account',{title:'Switch Accounts'})
+    res.render('./settings-views/switch-to-proffessional-account',{title:'Switch Accounts'})
 })
 
 module.exports = router
